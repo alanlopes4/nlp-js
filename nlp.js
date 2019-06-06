71872b91e947fac52a501c5f9e3940386bf654b4
Alan Lopes <alanlopes4@gmail.com>
2019-06-06 08:22:40 -0300

    refactor: removing comments

--- nlp.js
+++ nlp.js
@@ -1,36 +1,23 @@
-/* Any copyright is dedicated to the Public Domain.
- * http://creativecommons.org/publicdomain/zero/1.0/ */
-
-//
-// Basic node example that prints document metadata and text content.
-// Requires single file built version of PDF.js -- please run
-// `gulp singlefile` before running the example.
-//
-
-// Run `gulp dist-install` to generate 'pdfjs-dist' npm package files.
 var pdfjsLib = require("pdfjs-dist");
 
-// Loading file from file system into typed array
 var pdfPath = "../nlp-javascrit/pdfs/13.pdf";
 
-// Will be using promises to load document, pages and misc data instead of
-// callback.
 var loadingTask = pdfjsLib.getDocument(pdfPath);
 loadingTask.promise
   .then(function(doc) {
     var numPages = doc.numPages;
-    console.log("# Document Loaded");
-    console.log("Number of Pages: " + numPages);
+    console.log("# Documento carregador");
+    console.log("Número de Páginas: " + numPages);
     console.log();
 
-    var lastPromise; // will be used to chain promises
+    var lastPromise;
     lastPromise = doc.getMetadata().then(function(data) {
-      console.log("# Metadata Is Loaded");
+      console.log("# Metadados carregados");
       console.log("## Info");
       console.log(JSON.stringify(data.info, null, 2));
       console.log();
       if (data.metadata) {
-        console.log("## Metadata");
+        console.log("## Metadado");
         console.log(JSON.stringify(data.metadata.getAll(), null, 2));
         console.log();
       }
@@ -38,19 +25,17 @@ loadingTask.promise
 
     var loadPage = function(pageNum) {
       return doc.getPage(pageNum).then(function(page) {
-        console.log("# Page " + pageNum);
+        console.log("# Página " + pageNum);
         var viewport = page.getViewport({ scale: 1.0 });
-        console.log("Size: " + viewport.width + "x" + viewport.height);
+        console.log("Tamanho: " + viewport.width + "x" + viewport.height);
         console.log();
         return page
           .getTextContent()
           .then(function(content) {
-            // Content contains lots of information about the text layout and
-            // styles, but we need only strings at the moment
             var strings = content.items.map(function(item) {
               return item.str;
             });
-            console.log("## Text Content");
+            console.log("## Conteúdo do texto");
             console.log(strings.join(" "));
           })
           .then(function() {
@@ -58,8 +43,6 @@ loadingTask.promise
           });
       });
     };
-    // Loading of the first page will wait on metadata and subsequent loadings
-    // will wait on the previous pages.
     for (var i = 1; i <= numPages; i++) {
       lastPromise = lastPromise.then(loadPage.bind(null, i));
     }
@@ -67,7 +50,7 @@ loadingTask.promise
   })
   .then(
     function() {
-      console.log("# End of Document");
+      console.log("# fim do documento");
     },
     function(err) {
       console.error("Error: " + err);
