2031efa3223f7e23a5393db4745657c82fb944ce
Alan Lopes <alanlopes4@gmail.com>
2019-06-06 18:55:58 -0300

    feat: salving datas in array

--- NLP.js
+++ NLP.js
@@ -1,23 +1,32 @@
 var textract = require("textract");
 
-var filePath = "./pdfs/13.pdf";
+var filePaths = [
+  "13.pdf",
+  "17.pdf",
+  "19.pdf",
+  "20.pdf",
+  "21.pdf",
+  "22.pdf",
+  "23.pdf",
+  "29.pdf",
+  "21.pdf",
+  "46.pdf",
+  "48.pdf",
+  "49.pdf",
+  "50.pdf",
+  "98.pdf",
+  "118.pdf",
+  "120.pdf"
+];
 var config = {
   preserveLineBreaks: true
 };
 
 var idx_abstract, idx_index;
-
+var reg = /\[[0-9]+\]/g;
 var articles = [];
-var article = {
-  title: "",
-  references: [],
-  authors: [],
-  institutions: [],
-  abstract: [],
-  terms: []
-};
 
-async function extractTextFromPdf(filePath, config) {
+function extractTextFromPdf(filePath, config) {
   return new Promise((resolve, reject) => {
     textract.fromFileWithPath(filePath, config, function(error, text) {
       if (error) {
@@ -36,6 +45,14 @@ function getTitle(text) {
   return title;
 }
 
+function getAuthors(text) {
+  text.some((line, idx) => {
+    idx_abstract = idx;
+    return line.startsWith("ABSTRACT");
+  });
+  return text[idx_abstract - 1];
+}
+
 function getAbsctract(text) {
   let abstract = [];
   text.some((line, idx) => {
@@ -47,21 +64,53 @@ function getAbsctract(text) {
   return abstract.join("");
 }
 
-function getAuthors(text) {
-  text.some((line, idx) => {
-    idx_abstract = idx;
-    return line.startsWith("ABSTRACT");
-  });
-  return text[idx_abstract - 1];
+function getReferences(text) {
+  for (let i = idx_index; i < text.length; i++) {
+    if (text[i].startsWith("REFERENCES"))
+      return getLinesByRegExpression(text, i);
+  }
 }
 
-extractTextFromPdf("./pdfs/17.pdf", config)
-  .then(text => {
-    //remove first line
-    text.shift();
+function getLinesByRegExpression(text, idx) {
+  let references = [];
+  for (let i = idx + 1; i < text.length; i++) {
+    if (reg.test(text[i])) references.push(text[i]);
+  }
+  return references;
+}
 
-    article.authors = getAuthors(text);
-    article.title = getTitle(text);
-    article.abstract = getAbsctract(text);
-  })
-  .catch(error => console.log(error));
+function init() {
+  let num = 0;
+  filePaths.forEach(file => {
+    extractTextFromPdf("./pdfs/" + file, config)
+      .then(text => {
+        //remove first line
+        text.shift();
+
+        articles.push({
+          article: file,
+          title: getTitle(text),
+          authors: getAuthors(text),
+          institutions: [],
+          abstract: getAbsctract(text),
+          objective: "",
+          problem: "",
+          methodology: "",
+          contributes: "",
+          references: getReferences(text),
+          terms: []
+        });
+        num++;
+        generateGraph(num);
+      })
+      .catch(error => console.log(error));
+  });
+}
+
+function generateGraph(num) {
+  if (num == filePaths.length) {
+    console.log(articles);
+  }
+}
+
+init();
